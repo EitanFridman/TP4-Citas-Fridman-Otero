159105b9313974f390e616b9c4c0049825a46856
EITÁN GABRIEL FRIDMAN <48242472@ort>
2025-05-20 14:29:25 -0300

    local storage inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import Formulario from './Formulario';
 import ListadoCitas from './ListadoCitas';
 import './App.css';
 
 function App() {
   const [citas, setCitas] = useState([
-    {
-      id: "1",
-      mascota: "Nina",
-      propietario: "Martin",
-      fecha: "2021-08-05",
-      hora: "08:20",
-      sintomas: "Le duele la pierna"
-    },
-    {
-      id: "2",
-      mascota: "Sifon",
-      propietario: "Flecha",
-      fecha: "2023-08-05",
-      hora: "09:24",
-      sintomas: "Duerme mucho"
-    }
+    // {
+    //   id: "1",
+    //   mascota: "Nina",
+    //   propietario: "Martin",
+    //   fecha: "2021-08-05",
+    //   hora: "08:20",
+    //   sintomas: "Le duele la pierna"
+    // },
+    // {
+    //   id: "2",
+    //   mascota: "Sifon",
+    //   propietario: "Flecha",
+    //   fecha: "2023-08-05",
+    //   hora: "09:24",
+    //   sintomas: "Duerme mucho"
+    // }
   ]);
+
+  useEffect(() => {
+    const citasGuardadas = localStorage.getItem('citas');
+    if (citasGuardadas) {
+      setCitas(JSON.parse(citasGuardadas));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('citas', JSON.stringify(citas));
+  }, [citas]);
 
   const agregarCita = (nuevaCita) => {
     setCitas([...citas, {...nuevaCita, id: Date.now()}]);
