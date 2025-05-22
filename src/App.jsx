90c217c01681bdb89b0a5d6fc891b90dc69ae5e8
EITÁN GABRIEL FRIDMAN <48242472@ort>
2025-05-22 09:01:22 -0300

    localStorage hecho

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,10 @@ import ListadoCitas from './ListadoCitas';
 import './App.css';
 
 function App() {
-  const [citas, setCitas] = useState([
-    // {
-    //   id: "1",
-    //   mascota: "Nina",
-    //   propietario: "Martin",
-    //   fecha: "2021-08-05",
-    //   hora: "08:20",
-    //   sintomas: "Le duele la pierna"
-    // },
-    // {
-    //   id: "2",
-    //   mascota: "Sifon",
-    //   propietario: "Flecha",
-    //   fecha: "2023-08-05",
-    //   hora: "09:24",
-    //   sintomas: "Duerme mucho"
-    // }
-  ]);
-
-  useEffect(() => {
+  const [citas, setCitas] = useState(() => {
     const citasGuardadas = localStorage.getItem('citas');
-    if (citasGuardadas) {
-      setCitas(JSON.parse(citasGuardadas));
-    }
-  }, []);
+    return citasGuardadas ? JSON.parse(citasGuardadas) : [];
+  });
 
   useEffect(() => {
     localStorage.setItem('citas', JSON.stringify(citas));
